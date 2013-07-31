daaf92b82b947b741ae49fa52cad231380703963
Eric Savvy <eric.chen0121@gmail.com>
2013-07-30 20:57:28 -0700

    colored the boxes with jquery

--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){
 
+	$('#get_color').on('click', function(e){
+		e.preventDefault();
+
+		// or $.getJSON();
+		$.ajax({
+			url: '/color', 
+			type: 'POST',
+			data: {_accept: "application/json"}, 
+			datatype: 'json', 
+			success: function(data) {
+				$('#color_me').children().eq(data.cell).css('background', data.color);
+			}
+		});
+	});
 });
